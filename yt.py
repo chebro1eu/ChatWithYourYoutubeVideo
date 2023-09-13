8b8596194b721c0ca69ca1d87af0c40d2a7e9973
Chebrolu Niranjan <125688401+chebro1eu@users.noreply.github.com>
2023-09-13 05:51:17 +0530

    Updated yt.py

--- yt.py
+++ yt.py
@@ -1,4 +1,4 @@
-import os
+import re
 from creds import apikey
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import YoutubeLoader
@@ -15,6 +15,13 @@ import string
 import spacy
 from collections import Counter
 from openai.error import AuthenticationError
+from rake_nltk import Rake
+from langchain.schema.document import Document
+from sklearn.cluster import KMeans
+from transformers import BertTokenizer, BertModel
+import torch
+import numpy as np
+
 
 
 
@@ -27,7 +34,92 @@ st.title("Ask questions on your Youtube Video")
 
 url = st.text_input("Plug in your Youtube Video")
 
-openai_api_key = st.sidebar.text_input("Enter your OPENAI API Key",type="password",)
+openai_api_key = st.sidebar.text_input("Enter your OPENAI API Key",type="password")
+
+
+
+def extract_phrases(text):
+    text = text.lower()
+    filler_words = ["uh", "um", "like", "you know", "so", "actually", "basically", "seriously"]
+    for filler in filler_words:
+        text = text.replace(filler, "")
+
+    # Tokenization, Removing Stop Words, Non-Alphanumeric Characters, and Lemmatization
+    doc = nlp(text)
+    lemmatized_tokens = [token.lemma_ for token in doc if not token.is_stop and token.text.isalpha()]
+
+    # Rejoin Tokens
+    cleaned_text = ' '.join(lemmatized_tokens)
+    
+    model_name = "bert-base-uncased"
+    model = BertModel.from_pretrained(model_name)
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+
+    # Encode Transcript and Get BERT Embeddings
+    inputs = tokenizer(cleaned_text, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    embeddings = outputs.last_hidden_state.mean(dim=1)[0]
+
+    # Extract Phrases using SpaCy
+    doc = nlp(cleaned_text)
+    phrases = [chunk.text for chunk in doc.noun_chunks]
+
+    # If you have fewer than 10 phrases, you can stop here
+    if len(phrases) <= 10:
+        top_phrases = phrases
+    else:
+        # Use BERT embeddings to rank the phrases
+        phrase_embeddings = []
+        for phrase in phrases:
+            phrase_input = tokenizer(phrase, return_tensors="pt", truncation=True, padding=True, max_length=50)
+            with torch.no_grad():
+                phrase_output = model(**phrase_input)
+            phrase_embedding = phrase_output.last_hidden_state.mean(dim=1)[0]
+            phrase_embeddings.append(phrase_embedding.numpy())
+
+        # Cluster the phrase embeddings
+        kmeans = KMeans(n_clusters=10)
+        kmeans.fit(phrase_embeddings)
+        centroids = kmeans.cluster_centers_
+
+        top_phrases = []
+        for centroid in centroids:
+            distances = [np.linalg.norm(embedding - centroid) for embedding in phrase_embeddings]
+            top_phrase_idx = np.argmin(distances)
+            top_phrases.append(phrases[top_phrase_idx])
+            
+        return top_phrases
+
+    
+    
+    
+
+def get_important_keywords(url):
+    loader = YoutubeLoader.from_youtube_url(url,add_video_info = True)
+
+    
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 900,
+        chunk_overlap = 50,
+        )
+
+    docs = text_splitter.split_documents(transcript)
+    text = ""
+    for doc in docs:
+        text += doc.page_content
+        
+    
+    ##for doc in docs:
+        ##keywords = []
+        ##keywords = get_keywords(doc)
+    keywords = extract_phrases(text)
+    
+    return keywords
+    
+    
 
 
 def conv_ytvid_into_docs(url):
@@ -38,8 +130,8 @@ def conv_ytvid_into_docs(url):
     transcript = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 50,
+        chunk_size = 1300,
+        chunk_overlap = 250,
         )
 
     docs = text_splitter.split_documents(transcript)
@@ -64,24 +156,22 @@ def ask_questions_on_vid(db,query):
     docs = db.similarity_search(query)
     docs_page_content = " ".join([d.page_content for d in docs])
 
-    chat = ChatOpenAI(model_name = "gpt-3.5-turbo",temperature=0.0,openai_api_key= openai_api_key)
+    chat = ChatOpenAI(model_name = "gpt-4",temperature=0.0,openai_api_key= openai_api_key)
 
     template = """
-            You are a helpful assistant that can answer questions about youtube videos
-         based on the video's transcript: {docs}
-        
-            Only use the factual information from the transcript to answer the question
-        
-            If you feel like you do not have enough information just say, "I do not know the answer to this"
-        
-            Your answers should be verbose and detailed.
-            
-            Use proper Grammar Punctuation and allign your response into mulitple paragraphs if needed.
-            
-            Try to use less than 10 sentences to complete your response.
-            
-            Unless asked for do not give your response in points.
-        
+            You are a highly specialized AI developed for the specific task of summarizing YouTube transcripts : {docs} and answering user queries about them.
+            When provided with a YouTube transcript, you are to process it as if you've studied the content of the associated video in great depth.
+            You are to imagine that you've reviewed the transcript multiple times, taken detailed notes, and understood its content deeply.
+            You possess the ability to extract both general themes and specific details from these transcripts.
+
+            For every user question:
+
+            If the query is general, offer a comprehensive yet concise response that captures the key points from the transcript.
+            If the query is specific, answer with precision, focusing on the exact details mentioned in the transcript.
+            Always tailor your responses based on the content of the YouTube transcript and the nature of the user's question.
+
+            Prioritize accuracy and relevance in your answers. You should strive to answer as if you are an expert who has studied the transcript content thoroughly.
+            Remember, the goal is to offer users the experience of interacting with an expert who has just meticulously analyzed the transcript of the video they are inquiring about.
             """
 
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
@@ -102,22 +192,30 @@ def ask_questions_on_vid(db,query):
     response = proper_punctuation(response)
     return response
 
-def proper_punctuation(response):
-    desired_punctuation = "."
-    lines = response.split("\n")
-    formatted_lines = []
+def proper_punctuation(text):
+    # Rule 1: Add space after punctuation if not already present
+    text = re.sub(r'([.,!?;:])([^\s])', r'\1 \2', text)
     
-    for line in lines:
-        if line.strip() and line[-1] not in string.punctuation:
-            line += desired_punctuation
-        
-        formatted_lines.append(line)
-        
-    formatted_response = "\n".join(formatted_lines)
+    # Rule 2: Format lists
+    # Start list on a new line after colon or introductory text
+    text = re.sub(r':\s*(\d+\.)', r':\n\n\1', text)
+    # Ensure each list item starts on a new line and has a space after the period
+    text = re.sub(r'(\d+)\.\s*', r'\n\1. ', text)
     
-    return formatted_response
+    # Rule 3: Left-aligning is default in most environments, so no specific code is needed
+    
+    # Rule 4: Ensure single space between sentences
+    text = re.sub(r'\s{2,}', ' ', text)
+    
+    # Rule 5: Space after closing quotation
+    text = re.sub(r'"\s*([A-Z])', r'" \1', text)
+    
+    # Add extra newline before each list item
+    text = re.sub(r'(\d+\.)', r'\n\1', text)
+    
+    return text.strip()
 
-def get_keywords(response):
+###def get_keywords(response):
     doc = nlp(response)
     # Extract keywords (nouns and proper nouns)
     keywords = [token.text for token in doc if token.pos_ in ["NOUN", "PROPN"]]
@@ -128,6 +226,7 @@ def get_keywords(response):
     # Get the top 5 keywords
     top_keywords = [keyword for keyword, _ in keyword_freq.most_common(5)]
     return top_keywords
+###
 
 def get_video_suggestions(top_keywords):
     tool = YouTubeSearchTool()
@@ -150,7 +249,7 @@ if url:
         st.write(response)
         st.subheader("Do you want to generate similar videos?")
         if st.button("Generate"):
-            words = get_keywords(response)
+            words = get_important_keywords(url)
             get_video_suggestions(words)
         
         
